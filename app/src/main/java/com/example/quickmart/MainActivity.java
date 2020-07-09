6016d92e66508ede3e1481d6b1e8d5ec0e6b5047
Sukhmanjot Kaur Gill <gillmanjitsukhman@gmail.com>
2020-07-09 14:49:49 -0400

    second commit

--- app/src/main/java/com/example/quickmart/MainActivity.java
+++ app/src/main/java/com/example/quickmart/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.quickmart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+inflater.inflate(R.menu.menu, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.icon:
+                Toast.makeText(this, "Icon Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.settings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.Contact_Us:
+                Toast.makeText(this, "Contact Us Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+
+
+
     }
 }
