c4c638cf144d35f36244f2139a183fef384abb03
ripenpreet kaur <reetkhaira125@gmail.com>
2020-09-07 20:18:53 -0400

    changes updated ripen

--- app/src/main/java/com/example/quickmart/HomeActivity.java
+++ app/src/main/java/com/example/quickmart/HomeActivity.java
@@ -1,135 +1,53 @@
 package com.example.quickmart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.quickmart.ui.MenClothing;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-
-
-    private AppBarConfiguration mAppBarConfiguration;
-
+public class HomeActivity extends AppCompatActivity {
+    RecyclerView recview;
+    myadapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        recview=(RecyclerView)findViewById(R.id.recview);
+        //recview.setLayoutManager(new LinearLayoutManager(this));
 
+        // LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        //recview.setLayoutManager(layoutManager);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Dashboard");
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+        recview.setLayoutManager(gridLayoutManager);
 
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Men"), model.class)
+                        .build();
 
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Checkout", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        View headerView =navigationView.getHeaderView(0);
-        TextView userNameTextView=headerView.findViewById(R.id.user_profile_name);
-        CircleImageView profileImageView=headerView.findViewById(R.id.profile_image);
-
-
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_Feedback)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+        adapter=new myadapter(options);
+        recview.setAdapter(adapter);
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-        }
-
-         else if(id==R.id.nav_men){
-         startActivity(new Intent(HomeActivity.this,MenClothing.class));
-
-        }
-         else if (id == R.id.nav_settings) {
-
-        }
-
-         else if (id==R.id.nav_logout) {
-
-
-        }
-        DrawerLayout drawer=(DrawerLayout)findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
 
 
--- app/src/main/java/com/example/quickmart/ui/feedback/FeedBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.quickmart.ui.feedback;
-
-import androidx.lifecycle.ViewModel;
-
-public class FeedBack extends ViewModel {
-    // TODO: Implement the ViewModel
-}
